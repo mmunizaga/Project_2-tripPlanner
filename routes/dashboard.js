f9efb27b0589fe3a427e8195ac576e241c297242
N10T <andy.tisba@gmail.com>
2020-02-12 11:18:17 +0100

    CRUD is done

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -46,28 +46,12 @@ router.get("/my-trip/:id/edit-my-trip", (req, res) => {
   tripModel
     .findById(req.params.id)
     .then(trip => {
-      trip.cityOrigin.date =  moment(trip.cityOrigin.date).format("YYYY-MM-DD[T]HH:mm").toString()
-      trip.cityToVisit.date =  moment(trip.cityToVisit.date).format("YYYY-MM-DD[T]HH:MM").toString()
-  
-
       res.render("forms/edit-my-trip", { trip });
     })
     .catch(error => console.log(error));
 });
 
-router.post("/my-trip/:id/edit", (req, res) => {
-
-  tripModel
-    .findByIdAndUpdate(req.params.id)
-    .then(trip => {
-      res.redirect("/my-trip/:id");
-    })
-    .catch(error => console.log(error));
-});
-
-router.post("/create-a-trip", protectRoute, (req, res,next) => {
-  console.log(req.session.currentUser,"---------------------------------------------------");
-  
+router.post("/my-trip/:id/edit", protectRoute, (req, res) => {
   const trip_name = req.body.trip_name;
   const dateDepart = req.body.dateDepart;
   const dateArrive = req.body.dateArrive;
@@ -90,9 +74,69 @@ router.post("/create-a-trip", protectRoute, (req, res,next) => {
   const ticketUrl_go = req.body.ticketUrl_go;
   const ticket_price_go = req.body.ticket_price_go;
 
-console.log(dateArrive);
-console.log(Date(dateArrive));
+  // userOwner: [req.session.currentUser._id],
+  const data = {
+    title: trip_name,
+      cityOrigin: {
+      date: dateDepart,
+      city: fromCity,
+      country: fromCountry,
+      transport_type: transport_type_go,
+      transport_price: ticket_price_go,
+      transportUrl: ticketUrl_go
+    },
+    cityToVisit: {
+      date: dateArrive,
+      city: toCity,
+      country: toCountry,
+      transport_type: transport_type_return,
+      transport_price: ticket_price_return,
+      transportUrl: ticketUrl_return
+    },
+    accommodations: {
+      accommodations_url: accommodations_url,
+      accommodations_type: accommodations_type,
+      accommodations_price: accommodations_price,
+      accommodations_address: {
+        number: accommodations_number,
+        street: accommodations_street,
+        street_type: accommodations_street_type,
+        city: accommodations_city
+      }
+    },
+    activities: activity
+  };
 
+  tripModel
+    .findByIdAndUpdate(req.params.id, data)
+    .then(trip => {
+      res.redirect(`/my-trip/${req.params.id}`);
+    })
+    .catch(error => console.log(error));
+});
+
+router.post("/create-a-trip", protectRoute, (req, res,next) => {
+  const trip_name = req.body.trip_name;
+  const dateDepart = req.body.dateDepart;
+  const dateArrive = req.body.dateArrive;
+  const fromCity = req.body.fromCity;
+  const toCity = req.body.toCity;
+  const fromCountry = req.body.fromCountry;
+  const toCountry = req.body.toCountry;
+  const accommodations_type = req.body.accommodations_type;
+  const accommodations_number = req.body.accommodations_number;
+  const accommodations_street_type = req.body.accommodations_street_type;
+  const accommodations_street = req.body.accommodations_street;
+  const accommodations_city = req.body.accommodations_city;
+  const accommodations_url = req.body.accommodations_url;
+  const accommodations_price = req.body.accommodations_price;
+  const activity = req.body.activity;
+  const transport_type_go = req.body.transport_type_go;
+  const transport_type_return = req.body.transport_type_return;
+  const ticketUrl_return = req.body.ticketUrl_return;
+  const ticket_price_return = req.body.ticket_price_return;
+  const ticketUrl_go = req.body.ticketUrl_go;
+  const ticket_price_go = req.body.ticket_price_go;
 
   const data = {
     userOwner: [req.session.currentUser._id],
